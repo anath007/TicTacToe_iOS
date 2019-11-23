0b3e5fb53360704f9a9e7083e5536666ca656bd5
Akhil Nathoo <akhilnathoo123@gmail.com>
2019-11-23 10:52:08 -0500

    added grid and 'X' and 'O' functionality

--- TicTacToe_iOS/TicTacToe_iOS/ViewController.swift
+++ TicTacToe_iOS/TicTacToe_iOS/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var activePlayer = 1;
+    
+    var gameState=[0,0,0,0,0,0,0,0,0]
+    
+    
+    @IBAction func action(_ sender: AnyObject) {
+        if(gameState[sender.tag-1]==0){
+            gameState[sender.tag-1]=activePlayer
+        if(activePlayer==1)
+        {
+            sender.setImage(UIImage(named: "Cross.png"), for:UIControl.State())
+            activePlayer=2
+        }
+        else
+         {
+            sender.setImage(UIImage(named: "Nought.png"), for:UIControl.State())
+            activePlayer=1
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
