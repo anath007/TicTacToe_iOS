5ed007d9f17f57c57c625db3b02cd991930e3697
Akhil Nathoo <akhilnathoo123@gmail.com>
2019-11-23 09:07:32 -0500

    Inital setup

---
+++ TicTacToe_iOS/TicTacToe_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe_iOS
+//
+//  Created by Akhil Nathoo on 2019-11-23.
+//  Copyright © 2019 Akhil Nathoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
